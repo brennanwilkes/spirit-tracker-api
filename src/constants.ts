228e76bfe9233c54c33a7f44b68c2ec823bac3a2
Brennan Wilkes (Text Groove) <brennan@textgroove.com>
2026-02-16 12:29:28 -0800

    fix: Made token one week

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const ALLOWED_ORIGIN = 'https://spirit.codexwilkes.com';
 
 export const JSON_CT = 'application/json; charset=utf-8';
 
-export const JWT_TTL_SECONDS = 180 * 24 * 60 * 60; // 180 days
+export const JWT_TTL_SECONDS = 7 * 24 * 60 * 60; // One week
 
 export const RESOURCES = ['details', 'favourites', 'sampled', 'score'] as const;
 export type Resource = (typeof RESOURCES)[number];
